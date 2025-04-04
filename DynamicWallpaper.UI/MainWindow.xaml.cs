eb562d059beb3440b8a5661e89d1e371e5889bc8
Leandro Sousa <l34ndro32@gmail.com>
2025-04-04 12:49:18 -0300

    adição da funcionalidade para aplicação de wallpapers

--- DynamicWallpaper.UI/MainWindow.xaml.cs
+++ DynamicWallpaper.UI/MainWindow.xaml.cs
@@ -7,12 +7,26 @@ using DynamicWallpaper.Service;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.Animation;
 using DynamicWallpaper.UI.src.Screens;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Runtime.InteropServices;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace DynamicWallpaper.UI
 {
     public partial class MainWindow : Window
     {
+        private const int SPI_SETDESKWALLPAPER = 20;
+        private const int SPIF_UPDATEINIFILE = 0x01;
+        private const int SPIF_SENDCHANGE = 0x02;
+
         private readonly WallpaperService _wallpaperService;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        string wallpapersPath = "D:\\leo\\source\\repos\\DynamicWallpaper\\DynamicWallpaper.UI\\Assets\\Wallpapers";
+        private List<string> availableWallpapers = new List<string>();
 
         public MainWindow()
         {
@@ -22,27 +36,106 @@ namespace DynamicWallpaper.UI
 
         private void LoadWallpapers()
         {
-            string[] wallpapers = WallpaperManager.GetAvailableWallpapers();
-
-            foreach (string wallpaper in wallpapers)
+            if (Directory.Exists(wallpapersPath))
             {
-                var image = new Image()
-                {
-                    Source = new BitmapImage(new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "Wallpapers", wallpaper))),
-                    Width = 100,
-                    Height = 100,
-                    Margin = new Thickness(5),
-                    Cursor = System.Windows.Input.Cursors.Hand
-                };
+                string[] imageFiles = Directory.GetFiles(wallpapersPath, "*.*", SearchOption.TopDirectoryOnly)
+                                .Where(f => f.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                                           f.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
+                                .ToArray();
 
-                image.MouseLeftButtonUp += (s, e) => WallpaperManager.SetWallpaper(System.IO.Path.GetFileName(wallpaper));
-                WallpaperPanel.Children.Add(image);
+                availableWallpapers = imageFiles.ToList();
+
+                WallpaperPanel.Children.Clear();
+
+                double imageSize = Math.Max(200, Math.Min(this.ActualWidth / 5, 200));
+                 
+                foreach (var imagePath in imageFiles)
+                {
+                    DropShadowEffect shadowEffect = new DropShadowEffect
+                    {
+                        Color = Colors.Red,
+                        ShadowDepth = 0,
+                        BlurRadius = 50,
+                        Opacity = 0.5
+                    };
+
+                    Border border = new Border
+                    {
+                        Width = imageSize,
+                        Height = 170,
+                        Margin = new Thickness(5),
+                        BorderBrush = Brushes.Blue,
+                        BorderThickness = new Thickness(2),
+                        CornerRadius = new CornerRadius(10),
+                        ClipToBounds = true,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        Effect = shadowEffect,
+                        RenderTransformOrigin = new Point(0.5, 0.5)
+                    };
+
+                    TransformGroup transformGroup = new TransformGroup();
+                    ScaleTransform scaleTransform = new ScaleTransform();
+                    RotateTransform rotateTransform = new RotateTransform();
+                    transformGroup.Children.Add(scaleTransform);
+                    transformGroup.Children.Add(rotateTransform);
+                    border.RenderTransform = transformGroup;
+
+                    Image image = new Image
+                    {
+                        Width = imageSize,
+                        Height = imageSize
+                    };
+
+                    BitmapImage bitmap = new BitmapImage(new Uri(imagePath));
+                    image.Source = bitmap;
+
+                    border.MouseEnter += (sender, e) =>
+                    {
+                        var scaleAnimation = new DoubleAnimation(1, 1.1, TimeSpan.FromSeconds(0.3));
+                        scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+                        scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+
+                        var rotateAnimation = new DoubleAnimation(0, 10, TimeSpan.FromSeconds(0.3));
+                        rotateTransform.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
+
+                        var shadowAnimation = new DoubleAnimation(3, 10, TimeSpan.FromSeconds(0.3));
+                        shadowEffect.BeginAnimation(DropShadowEffect.ShadowDepthProperty, shadowAnimation);
+                    };
+
+                    border.MouseLeave += (sender, e) =>
+                    {
+                        var scaleAnimation = new DoubleAnimation(1.1, 1, TimeSpan.FromSeconds(0.3));
+                        scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+                        scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+
+                        var rotateAnimation = new DoubleAnimation(10, 0, TimeSpan.FromSeconds(0.3));
+                        rotateTransform.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
+
+                        var shadowAnimation = new DoubleAnimation(10, 3, TimeSpan.FromSeconds(0.3));
+                        shadowEffect.BeginAnimation(DropShadowEffect.ShadowDepthProperty, shadowAnimation);
+                    };
+
+                    border.MouseLeftButtonUp += (sender, e) =>
+                    {
+                        // Ao clicar, aplicar o wallpaper correspondente
+                        SetWallpaper(imagePath);
+                    };
+
+                    border.Child = image;
+
+                    WallpaperPanel.Children.Add(border);
+                }
+
+                WallpaperPanel.Opacity = 1.0;
+            }
+            else
+            {
+                MessageBox.Show("Deu erro ze");
             }
         }
 
         private void RefreshWallpapers(object sender, RoutedEventArgs e)
         {
-            // Criando animação de fade-out antes da atualização
             DoubleAnimation fadeOutAnimation = new DoubleAnimation
             {
                 From = 1,
@@ -53,7 +146,6 @@ namespace DynamicWallpaper.UI
 
             fadeOutAnimation.Completed += (s, _) =>
             {
-                // Limpa os wallpapers e recarrega a lista
                 WallpaperPanel.Children.Clear();
                 LoadWallpapers();
             };
@@ -82,33 +174,51 @@ namespace DynamicWallpaper.UI
 
         private void SelectWallpaper(object sender, RoutedEventArgs e)
         {
+            if (!Directory.Exists(wallpapersPath))
+            {
+                MessageBox.Show("A pasta de wallpapers não foi encontrada!", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var imageFiles = Directory.GetFiles(wallpapersPath, "*.*")
+                          .Where(f => f.EndsWith(".jpg") || f.EndsWith(".png"))
+                          .ToList();
+
+            if (imageFiles.Count == 0)
+            {
+                MessageBox.Show("Nenhum wallpaper encontrado!", "Erro", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
-                Filter = "Image Files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png",
-                Title = "Select Wallpaper"
+                InitialDirectory = wallpapersPath,
+                Filter = "Imagens|*.jpg;*.png",
+                Multiselect = false
             };
 
-            if(openFileDialog.ShowDialog() == true)
+            if (openFileDialog.ShowDialog() == true)
             {
-                AnimateWallpaperTransiction(() => _wallpaperService.SetSpecificWallpaper(openFileDialog.FileName));
-
-                PreviewWindow previewWindow = new PreviewWindow(openFileDialog.FileName, _wallpaperService);
-                previewWindow.ShowDialog();
+                SetWallpaper(openFileDialog.FileName);
             }
         }
 
-        private void AnimateWallpaperTransiction(Action setWallpaperAction)
+/*        private void ApplyWallpaper(object sender, RoutedEventArgs e)
         {
-            DoubleAnimation fadeOut = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(0.5));
-            DoubleAnimation fadeIn = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(0.5));
-
-            fadeOut.Completed += (s, e) =>
+            if (WallpaperList.SelectedItem != null)
             {
-                setWallpaperAction.Invoke();
-                WallpaperPanel.BeginAnimation(UIElement.OpacityProperty, fadeIn);
-            };
-
-            this.BeginAnimation(Window.OpacityProperty, fadeOut);
+                string selectedWallpaper = Path.Combine(wallpapersPath, WallpaperList.SelectedItem.ToString());
+                SetWallpaper(selectedWallpaper);
+            }
+            else
+            {
+                MessageBox.Show("deu erro ze", "Erro", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+*/
+        private void SetWallpaper(string filePath)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, filePath, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
         }
     }
 }
